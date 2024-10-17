d165493347282b72ed1255202bdbae66f2fc7711
ANONYMOUS <mmuhammadfauzi96@gmail.com>
2024-10-17 22:29:22 +0700

    first commit

---
+++ src/SelectionSort.java
@@ -0,0 +1,34 @@
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            int min_idx = i;
+            for (int j = i+1; j < n; j++) {
+                if (arr[j] < arr[min_idx]) {
+                    min_idx = j;
+                }
+            }
+            // Tukar elemen terkecil dengan elemen pertama yang belum terurut
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        System.out.println("Array sebelum diurutkan:");
+        printArray(arr);
+
+        selectionSort(arr);
+        System.out.println("Array setelah diurutkan:");
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
